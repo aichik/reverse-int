61f37c1354fff10592ba04c442514f463eab1511
Vartanyan Aik Artsrynievich <47832390+aichik@users.noreply.github.com>
2022-06-28 21:37:06 +0400

    feat: add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  n = n + '';
+  let arr = [];
+  for (let i = 0; i < n.length; i++) {
+    arr.unshift(n[i]);
+  }
+  if (arr[arr.length - 1] == '-') {
+    arr.pop(-1);
+  }
+  n = arr.join('');
+  return n;
 }
